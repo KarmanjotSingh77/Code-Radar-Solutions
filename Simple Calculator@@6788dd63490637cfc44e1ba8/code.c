46c53acd3af2193587ae352782b1185ab8a1544d
KarmanjotSingh77 <karmanjot1181.becse24@chitkara.edu.in>
2025-02-05 18:07:10 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -12,7 +12,11 @@ int main() {
     } else if (oprtr=='*') {
         num3=num1*num2;
     } else if (oprtr=='/') {
-        num3=num1/num2;
+        if (num2==0) {
+            printf("error");
+        } else {
+            num3=num1/num2;
+        }
     } 
     printf("%.0f",num3);
     return 0;
